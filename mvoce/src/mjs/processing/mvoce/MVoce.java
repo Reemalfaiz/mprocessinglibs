9f1c3f3a4d2d5308db59dd496854b558b98dd6a9
m4rlonj <m4rlonj@e278b022-b425-11dd-ac4e-d12b35b862f6>
2009-01-30 17:54:03 +0000

    MVoce : Speech Synthesis and Recognition, release 0.1

---
+++ mvoce/src/mjs/processing/mvoce/MVoce.java
@@ -0,0 +1,83 @@
+/*
+
+	MVoce - Voice Library for Processing
+
+	Copyright (c) 2009 Mary Jane Soft - Marlon J. Manrique
+	
+	http://www.maryjanesoft.com
+	http://www.marlonj.com
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General
+	Public License along with this library; if not, write to the
+	Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
+
+	$Id$
+	
+*/
+
+package mjs.processing.mvoce;
+
+/**
+ * Allows synthesize voice or recognize speech from a grammar 
+ */ 
+public abstract class MVoce
+{
+	/**
+	 * Requests that the given string be synthesized as soon as possible. 
+	 * 
+	 * @param message The message to sinthesize
+	 */
+	public abstract void synthesize(String message);
+	
+	/**
+	 * Tells the speech synthesizer to stop synthesizing. 
+	 * This cancels all pending messages.
+	 */
+	public abstract void stopSynthesizing();
+	
+	/**
+    * Returns the number of recognized strings currently in the recognizer's 
+    * queue.
+    * 
+    * @return Number of tokens recognized
+    */      	
+	public abstract int numTokens();
+	
+	/**
+	 * Returns and removes the oldest recognized string from the recognizer's 
+	 * queue.
+	 *
+	 * @return oldest recognized string
+	 */ 
+	public abstract String nextToken();
+
+	/**
+	 * Enables and disables the speech recognizer. 
+	 *
+	 * @param e true to enable the recognizer, false other wise
+	 */	
+	public abstract void recognizerEnable(boolean e);
+	
+	/**
+	 * Returns true if the recognizer is currently enabled. 
+	 *
+	 * @return true if the recognizer is currently enabled, false otherwise
+	 */ 
+	public abstract boolean isRecognizerEnabled();
+	
+	/**
+	 * Free resources 
+	 */
+	public abstract void destroy();
+}
